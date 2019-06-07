c3608a59920d37bc27b551df6c9d75198b851dd8
ShererStress <ryanswhitehill@gmail.com>
2019-06-06 21:59:33 -0600

    CORS related gem installed. Index route for users functional and returning data properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,35 @@
 class User
 
 
+  DB = PG.connect({
+    :host => "localhost",
+    :port => 5432,
+    :dbname => 'plane-rails_development'
+    });
+
+    #Prepared statements... in progress
+    DB.prepare("users_show_single",
+      <<-SQL
+        SELECT users.*
+        FROM users
+        WHERE users.id = $1;
+      SQL
+    )
+
+
+    # Routes... in progress
+
+    #GET: Index of users - returns array of user objects
+    def self.all
+      results = DB.exec("SELECT * FROM users;");
+      usersList = [];
+      results.each do |result|
+        usersList.push(result);
+
+      end
+      puts usersList;
+      return usersList;
+    end
+
 
 end #End user class
