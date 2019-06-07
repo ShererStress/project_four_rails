d95057bf96087a4b83db662c6e3747f95269448c
ShererStress <ryanswhitehill@gmail.com>
2019-06-06 22:47:25 -0600

    Users create and delet routes functional with prepared statements

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User
     });
 
     #Prepared statements... in progress
+
+    #GET: A user by id
     DB.prepare("users_find",
       <<-SQL
         SELECT users.*
@@ -16,6 +18,23 @@ class User
       SQL
     )
 
+    #POST: A new user
+    DB.prepare("users_create",
+      <<-SQL
+        INSERT INTO users (username, password)
+        VALUES ($1, $2)
+        RETURNING id, username, password;
+      SQL
+    )
+
+    #DELETE: An existing user
+    DB.prepare("users_delete",
+      <<-SQL
+        DELETE FROM users
+        WHERE users.id = $1
+        RETURNING id;
+      SQL
+    )
 
     # Routes... in progress
 
@@ -30,10 +49,33 @@ class User
       return usersList;
     end
 
+    #GET: A user by id
     def self.find(id)
       results = DB.exec_prepared("users_find", [id]);
       return results.first;
     end
 
+    #POST: A new user
+    #Add encryption here?
+    def self.create(options)
+      newUsername = options["username"];
+      newPassword = options["password"];
+
+      result = DB.exec_prepared("users_create", [newUsername, newPassword]);
+
+      return result.first
+    end
+
+    #DELETE: An existing user
+    def self.delete(id)
+      results = DB.exec_prepared("users_delete", [id]);
+      return results.first;
+    end
+
+
+    #PUT: Edit an existing user
+    def self.update()
+
+    end
 
 end #End user class
