9fb46230b20f5e351a6c087639d60e3e5cbfb80f
ShererStress <ryanswhitehill@gmail.com>
2019-06-10 15:54:04 -0600

    Added defualt route for troubleshooting purposes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  #Default route (troubleshooting)
+  get '/', to: 'users#index'
+
   # User routes
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
